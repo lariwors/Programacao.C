c895e265f2baa6e8ddaae11d3d357c32dec5a1e2
Larissa da Silva Lima <lariwors13@outlook.com>
2025-04-25 01:46:42 -0300

    Update and rename super_trunfo_Lv2.c to supertrunfo_aventureiro.c

--- supertrunfo_aventureiro.c
+++ supertrunfo_aventureiro.c
@@ -2,7 +2,7 @@
 //Iniciando o nível aventureiro para a criação do jogo: Super Trunfo - Países.
 
 int main (){
-    printf("Super Trunfo! - Desafio Aventureiro.\n");
+    printf("Super Trunfo! - Desafio Novato.\n");
 
 // Serão inseridas apenas duas cartas do jogo por enquanto, com as variáveis abaixo:
     char estado1, estado2; 
@@ -12,10 +12,6 @@ int main (){
     float area1, area2; //área em Km².
     float PIB1, PIB2;
     int pontos_turisticos1, pontos_turisticos2;
-    
-    //variáveis calculadas na conversão explícita.
-    float densidade_pop1, densidade_pop2; 
-    float PIB_per_capita1, PIB_per_capita2; 
 
 //Entrada da carta 1:
     printf("\nDigite os dados da primeira carta:\n");
@@ -53,10 +49,6 @@ int main (){
     printf("Pontos turísticos: ");
         scanf("%d", &pontos_turisticos2);
 
-// Cálculo utilizando a conversão explícita na carta1 (float)
-   densidade_pop1 = (float) populacao1 / area1; 
-   PIB_per_capita1 = (float) PIB1 * 1000000000 / (float) populacao1;
-
 //Saída da carta 1:
     printf("\n---- Carta 1 ----\n");
     printf("Estado: %c\n", estado1);
@@ -64,14 +56,8 @@ int main (){
     printf("Nome da cidade: %s\n", cidade1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f Km²\n", area1);
-    printf("Densidade populacional: %.2f hab/km²\n", densidade_pop1 );
     printf("PIB: %.2f bilhões de reais\n", PIB1);             
-    printf("PIB per capita: %.2f reais\n", PIB_per_capita1);
     printf("Pontos turísticos: %d\n\n", pontos_turisticos1);
-
-    // Cálculo utilizando a conversão explícita na carta2 (float)
-    densidade_pop2 = (float) populacao2 / area2;
-    PIB_per_capita2 = (float) PIB2 * 1000000000 / (float) populacao2;
 
 //Saída da carta 2.
     printf("---- Carta 2 ----\n");
@@ -80,9 +66,7 @@ int main (){
     printf("Nome da cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f Km²\n", area2);
-    printf("Densidade populacional: %.2f hab/km²\n", densidade_pop2);
     printf("PIB: %.2f bilhões de reais\n", PIB2);    
-    printf("O PIB per capita: %.2f reais\n", PIB_per_capita2);
     printf("Pontos turísticos: %d\n\n", pontos_turisticos2);
 
 return 0;
